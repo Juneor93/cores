5ab174e58778a591007cf423983a83d39d4e3e1a
Suelen Alves <suelen@MacBook-Pro-de-Suelen.local>
2020-09-03 11:35:51 -0300

    versao final

---
+++ build/precache-manifest.a328b0e1e963a2ebaa06525dad8eef67.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06534f78e62c4bf7aa7b75977046fad2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9935b643c712ee540b20",
+    "url": "/static/css/main.6a379075.chunk.css"
+  },
+  {
+    "revision": "ae609f3a2b3e93a478e0",
+    "url": "/static/js/2.74bc818f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.74bc818f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9935b643c712ee540b20",
+    "url": "/static/js/main.97d5aab0.chunk.js"
+  },
+  {
+    "revision": "4ad25b4dd31815097238",
+    "url": "/static/js/runtime-main.398507a5.js"
+  },
+  {
+    "revision": "93d0d6f28183056dafbd6851a8e34ec4",
+    "url": "/static/media/logo-paleta.93d0d6f2.png"
+  }
+]);
